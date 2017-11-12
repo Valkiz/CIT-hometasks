52a7c603bd26c23bafe9fcd766971e9f1971569a
Valkiz <32544589+Valkiz@users.noreply.github.com>
2017-11-13 01:47:10 +0300

    Add files via upload

--- Calculations.py
+++ Calculations.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 import requests
 
 OUR_USER = 13
+WORKING_DAYS = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri')
 
 def getTopFive(sim:list):
     top5 = []  # Индексы похожих юзеров
@@ -29,6 +30,12 @@ with open('data.csv', newline='') as csvfile:
     for a in data:
         print(','.join(a))
     '''
+with open('context.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+    raw_data = [raw_data[1:] for raw_data in reader][1:]
+    cont = np.asarray(raw_data) #Прочли в матрицу
+    #print(cont[OUR_USER])
+
 sim = [0]*40
 average = [0] * 40
 #Рассматриваем нашего пользователя (14)
@@ -69,6 +76,9 @@ top5 = getTopFive(sim)
 
 film = 0
 firstTask = {}
+films = [] # Один из этих фильмов мы порекомендуем
+maxGrade = 0
+maxGradeI=0
 for val in data[OUR_USER]:
     if int(val) == -1: #Нашли в строке неоцененный фильм
         data[OUR_USER][film] = 0 #Не забыли сбросить перед дальнейшими вычислениями
@@ -80,17 +90,105 @@ for val in data[OUR_USER]:
         data[OUR_USER][film] /= simBoth
         data[OUR_USER][film] += average[OUR_USER]
 
+        films.append(film) #для рекомендации
+        if data[OUR_USER][film] > maxGrade:
+            maxGrade = data[OUR_USER][film]
+            maxGradeI = film
+
         firstTask["movie " + str(film+1)] = round(data[OUR_USER][film],2)
         print('"movie ' + str(film+1)+'": ' +str(round(data[OUR_USER][film],2)))
     film+=1
-
+#print(data[OUR_USER])
 print(firstTask)
+
+MathW = {} # в рабочие дни
+MathH = {} # в выходные дни
+reliable={} #во сколько раз фильм интереснее в будние, чем в выходные
+nonReliable={} #во сколько раз фильм интереснее в будние, чем в выходные
+for f in films:
+    MathW["M"] = 0 #Мат.ожидание
+    MathW["M2"] = 0  # Мат.ожидание квадрата
+    MathW["C"] = 0  # колво просмотров
+    MathH["M"] = 0 #Мат.ожидание
+    MathH["M2"] = 0  # Мат.ожидание квадрата
+    MathH["C"] = 0  # колво просмотров
+    u=0
+    for user in data:
+        if u == OUR_USER:
+            u+=1
+            continue
+        if user[f] > 0:
+            if cont[u][f] in WORKING_DAYS:
+                MathW["C"]+=1
+                MathW["M"]+=user[f]
+                MathW["M2"]+= user[f] ** 2
+            else:
+                MathH["C"] += 1
+                MathH["M"] += user[f]
+                MathH["M2"] += user[f] ** 2
+        u+=1
+    MathW["M"] = round(MathW["M"]/MathW["C"],2)
+    MathH["M"] = round(MathH["M"]/MathH["C"],2)
+    MathW["M2"] = round(MathW["M2"] / MathW["C"], 2)
+    MathH["M2"] = round(MathH["M2"] / MathH["C"], 2)
+
+    MathW["Q"] = round(math.sqrt(MathW["M2"]-MathW["M"]**2),2) #Среднекв отклонение в будние дни
+    MathH["Q"] = round(math.sqrt(MathH["M2"] - MathH["M"] ** 2),2) #Среднекв отклонение в целом
+
+    #Оценка достоверности случайной величины
+    rel=abs(MathW["M"]-MathH["M"])/math.sqrt((MathW["Q"]/math.sqrt(MathW["C"]-1))**2+(MathH["Q"]/math.sqrt(MathH["C"]-1))**2)
+    if rel >= 2: #Величина достоверна
+        reliable[f]=MathW["M"]/MathH["M"]
+        reliable[f] *= reliable[f] #Насколько фильм интереснее для просмотра в будние
+    else:
+        nonReliable[f]=MathW["M"]/MathH["M"]
+        nonReliable[f] *= nonReliable[f]
+'''
+    print(str(MathW["M"]) + " " + str(MathH["M"]), end = ' | ')
+    print(str(MathW["Q"]) + " " + str(MathH["Q"]), end=' | ')
+    print(MathW["C"]/MathH["C"]/5*2)
+    print(MathW["C"], end=' ')
+    print(rel)
+'''
+
+def bestMovie(film_list):
+    mx = 0
+    mxi = 0
+    for ind, item in film_list.items():
+        if item>mx:
+            mx = item
+            mxi = ind
+    if mx >= 3:
+        return mxi
+    else:
+        return -1
+def goodMovie(film_list):
+    best = bestMovie(nonReliable)
+    if best == -1:
+        best = maxGradeI  # мы сдались
+    return best
+
+
+best = -1
+if len(reliable) > 0:
+    best = bestMovie(reliable)
+    if best == -1:
+
+        best = goodMovie(nonReliable)
+else:
+    best = goodMovie(nonReliable)
+
+print(best+1)
+
+
 reg_a = 'https://cit-home1.herokuapp.com/api/rs_homework_1'
 jsargs = {
     "user":14,
-    "1": firstTask
+    "1": firstTask,
+    "2": {"movie " + str(best+1) : firstTask["movie "+str(best+1)]}
 }
 head = {'content-type': 'application/json'}
 print()
-#r = requests.post(reg_a, json=jsargs,headers=head)
-#print(r.json())
+
+r = requests.post(reg_a, json=jsargs,headers=head)
+print(r.json())
