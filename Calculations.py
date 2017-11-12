3ad490af9bb6c8f02a0b896866ea0f4d285d5fa1
Valkiz <32544589+Valkiz@users.noreply.github.com>
2017-11-12 23:17:29 +0300

    Add files via upload

--- Calculations.py
+++ Calculations.py
@@ -7,6 +7,19 @@ import requests
 
 OUR_USER = 13
 
+def getTopFive(sim:list):
+    top5 = []  # Индексы похожих юзеров
+    simGrades = list(sim)
+    simGrades.sort(reverse=True)
+    taken = 0
+    for a in simGrades:
+        whichUser = sim.index(a)
+        while whichUser in top5:
+            whichUser = sim.index(a, whichUser + 1)
+        top5.append(whichUser)
+        taken += 1
+        if taken == 5: break
+    return top5
 
 with open('data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -42,58 +55,40 @@ for user in data:
     sim[u]=(A/math.sqrt(B))/math.sqrt(C)
 
     u += 1
-simGrades = list(sim)
-simGrades.sort(reverse=True)
 
-
-def indices(searchIn:list, searchWhat:list) :
-    for i in searchWhat :
-        res = searchIn.index(i)
-        searchIn.remove(i)
-        yield res
 
 sim = [round(a,4) for a in sim]
-simGrades = [round(a, 4) for a in simGrades]
+average = [round(a, 4) for a in average]
+
+# Берем топ5
+top5 = getTopFive(sim)
+
+# top5 = [32,14,37,33,36]
+#print("influenced by " + str([a + 1 for a in top5]))
+#print([sim[a] for a in top5])
 
 film = 0
+firstTask = {}
 for val in data[OUR_USER]:
     if int(val) == -1: #Нашли в строке неоцененный фильм
+        data[OUR_USER][film] = 0 #Не забыли сбросить перед дальнейшими вычислениями
         simBoth = 0
-        #print(sim)
-        #Берем топ5, оценивших тот же самый фильм
-        top5 = []  # Индексы похожих юзеров
-        taken=0
-        for a in simGrades:
-            whichUser = sim.index(a)
-            while whichUser in top5:
-                whichUser = sim.index(a,whichUser+1)
-            if data[whichUser][film] != -1:
-                top5.append(whichUser)
-                taken+=1
-            if taken == 5: break
-        print([sim[a] for a in top5])
-        print ("influenced by "+str([a+1 for a in top5]))
         for simUser in top5:
-            print(data[simUser][film])
-            data[OUR_USER][film]+= sim[simUser] * (data[simUser][film] - average[simUser])
-            simBoth+=abs(average[simUser])
+            if int(data[simUser][film]) != -1:
+                simBoth += abs(sim[simUser])
+                data[OUR_USER][film]+= sim[simUser] * (data[simUser][film] - average[simUser])
         data[OUR_USER][film] /= simBoth
         data[OUR_USER][film] += average[OUR_USER]
+
+        firstTask["movie " + str(film+1)] = round(data[OUR_USER][film],2)
         print('"movie ' + str(film+1)+'": ' +str(round(data[OUR_USER][film],2)))
     film+=1
-for a in data[OUR_USER]:
-    print(round(a,2), end=' ')
 
+print(firstTask)
 reg_a = 'https://cit-home1.herokuapp.com/api/rs_homework_1'
 jsargs = {
     "user":14,
-    "1": {
-"movie 4": 2.97,
-"movie 8": 2.78,
-"movie 9": 2.85,
-"movie 16": 2.96,
-"movie 26": 3.12
-    }
+    "1": firstTask
 }
 head = {'content-type': 'application/json'}
 print()
